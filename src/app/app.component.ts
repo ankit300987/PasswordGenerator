035c8bc83306b898dadfeca9546c0413f57a5e70
Ankit Srivastava <ankitsrivastava.fsd@gmail.com>
2021-08-10 17:16:57 +0530

    Fixed the Build issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Console } from 'console';
 import { PasswordGenerator } from 'src/Model/password-generator.model';
 import { RandomPasswordService } from './random-password.service';
 
