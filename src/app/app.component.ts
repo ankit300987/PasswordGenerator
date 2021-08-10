c8b66caf255010fccb6f9ea7d31205fe0e08f644
Ankit Srivastava <Ankit_Srivastava7@DellTeam.com>
2021-08-10 11:28:56 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PasswordGenerator } from 'src/Model/password-generator.model';
+import { RandomPasswordService } from './random-password.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PasswordGenerator';
+  private useLetters: boolean = false;
+  private useNumbers: boolean = false;
+  private useSymbols: boolean = false;
+  private passwordLength: number = 0;
+
+  generatedPassword: string = "";
+  private passwordGenerator : PasswordGenerator = new PasswordGenerator();
+  constructor(
+    private randomPasswordService : RandomPasswordService){
+
+  }
+
+  getTheLength(value) {
+    this.passwordLength = parseInt(value);
+    this.passwordGenerator.passwordLength = this.passwordLength;
+  }
+  generatePassword() {   
+    this.generatedPassword = this.randomPasswordService.generateRandomPassword(this.passwordGenerator);
+    console.log(this.generatedPassword);
+  }
+
+  getCheckBoxValue(evnt: any) {
+
+    switch (evnt.target.id) {
+      case ("useLetters"):
+        this.useLetters = evnt.target.checked;
+        this.passwordGenerator.useLetters =  this.useLetters ;
+        break;
+      case ("useNumbers"):
+        this.useNumbers = evnt.target.checked;
+        this.passwordGenerator.useNumbers =  this.useNumbers;
+        break;
+      case ("useSymbols"):
+        this.useSymbols = evnt.target.checked;
+        this.passwordGenerator.useSymbols = this.useSymbols;
+        break;
+    }
+  }
 }
